a350e447e9addf44bf1e5f9f02dd7f65b68cfe27
frank paulsen <frobnic+ita@gmail.com>
2019-08-30 13:34:23 +0200

    inserted code

--- 0704A1Eratosthenes/Program.cs
+++ 0704A1Eratosthenes/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _0704A1Eratosthenes
 {
     class Program
     {
+        static void anzeigen(int[] a)
+        {
+            for (int i = 2; i < a.Length; i++)
+            {
+                if (a[i] == 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+            Console.WriteLine("");
+        }
+
+        static void streichen(int[] a, int b)
+        {
+            for (int i = 2*b; i < a.Length; i += b)
+                a[i] = 1;
+        }
+
         static void Main(string[] args)
         {
+            int i = 2, j = 2;
+
+            int[] sieb = new int[1000]; 
+
+            do
+            {
+                if (sieb[i]==0)
+                    streichen(sieb, i);
+                anzeigen(sieb);
+                i++;
+            }
+            while (i < 37);
+
         }
     }
 }
