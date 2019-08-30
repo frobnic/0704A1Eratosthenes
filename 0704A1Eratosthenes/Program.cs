e2d7fca019b58d003f49e83911040da5faf01add
frank paulsen <frobnic+ita@gmail.com>
2019-08-30 13:54:27 +0200

    prettyfied something

--- 0704A1Eratosthenes/Program.cs
+++ 0704A1Eratosthenes/Program.cs
@@ -12,30 +12,34 @@ namespace _0704A1Eratosthenes
                 {
                     Console.Write("{0} ", i);
                 }
+                else
+                {
+                    Console.Write("-- ");
+                }
             }
+            Console.WriteLine("");
             Console.WriteLine("");
         }
 
         static void streichen(int[] a, int b)
         {
-            for (int i = 2*b; i < a.Length; i += b)
+            for (int i = 2 * b; i < a.Length; i += b)
                 a[i] = 1;
         }
 
         static void Main(string[] args)
         {
-            int i = 2, j = 2;
-
-            int[] sieb = new int[1000]; 
+            int i = 2;
+            int[] sieb = new int[1000];
 
             do
             {
-                if (sieb[i]==0)
-                    streichen(sieb, i);
                 anzeigen(sieb);
+                if (sieb[i] == 0)
+                    streichen(sieb, i);
                 i++;
             }
-            while (i < 37);
+            while (i * i < 1000);
 
         }
     }
